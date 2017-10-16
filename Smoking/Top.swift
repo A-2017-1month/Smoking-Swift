cc3a58c1d47ea67493f862de44cce57029b9bc3b
getty104 <hayabusatoshihumi@gmail.com>
2017-10-16 17:02:52 +0900

    ストーリーボードの作成

---
+++ Smoking/Top.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  Smoking
+//
+//  Created by 清野隼史 on 2017/10/16.
+//  Copyright © 2017年 清野隼史. All rights reserved.
+//
+
+import UIKit
+
+class TopController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
+
